7547fd46de923e4b150c363abf2ce6c410608d3f
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 15:02:29 +0300

    updated link to github repo with todoa app

--- install-app.sh
+++ install-app.sh
@@ -12,8 +12,7 @@ apt-get install python3-pip -yq
 
 # Create a directory for the app and download the files. 
 mkdir /app 
-# make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
+git clone https://github.com/zave52/devops_todolist_terraform_task.git
 cp -r devops_todolist_terraform_task/app/* /app
 
 # create a service for the app via systemctl and start the app
