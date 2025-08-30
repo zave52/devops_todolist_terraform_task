da69c7d1e5e3359552c38dd2ddb827609f8e5a9c
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 15:44:20 +0300

    removed vm_public_ip and vm_fqdn output values from compute module

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -13,19 +13,9 @@ output "vm_private_ip" {
   value       = azurerm_network_interface.nic.private_ip_address
 }
 
-output "vm_public_ip" {
-  description = "The public IP address of the virtual machine"
-  value       = azurerm_network_interface.nic.ip_configuration[0].public_ip_address_id
-}
-
 output "network_interface_id" {
   description = "The ID of the network interface"
   value       = azurerm_network_interface.nic.id
-}
-
-output "vm_fqdn" {
-  description = "The fully qualified domain name of the virtual machine"
-  value       = azurerm_virtual_machine.vm.name
 }
 
 output "vm_size" {
