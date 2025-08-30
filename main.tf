47f10563cf365afb77375f0a7eb3146499446ba1
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 15:05:52 +0300

    added main variable and outputs terraform files

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "47216085-404b-4ca1-ba04-e2db06a084f3"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_container" "storage_container_for_backend" {
+  name                  = "tfstate"
+  storage_account_id    = module.storage.storage_account_id
+  container_access_type = "private"
+}
+
+module "storage" {
+  source = "./modules/storage"
+
+  location                         = azurerm_resource_group.rg.location
+  resource_group_name              = azurerm_resource_group.rg.name
+  storage_account_name             = var.storage_account_name
+  storage_account_tier             = "Standard"
+  storage_account_replication_type = "LRS"
+  storage_container_name           = var.storage_container_name
+}
+
+module "network" {
+  source = "./modules/network"
+
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  virtual_network_name        = var.virtual_network_name
+  vnet_address_prefix         = var.vnet_address_prefix
+  subnet_name                 = var.subnet_name
+  subnet_address_prefix       = var.subnet_address_prefix
+  network_security_group_name = var.network_security_group_name
+  public_ip_address_name      = var.public_ip_address_name
+  public_ip_allocation_method = "Static"
+  dns_label                   = var.dns_label
+  public_ip_sku               = "Standard"
+}
+
+module "compute" {
+  source = "./modules/compute"
+
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  vm_name                   = var.vm_name
+  vm_size                   = var.vm_size
+  ssh_key_public            = var.ssh_key_public
+  subnet_id                 = module.network.subnet_id
+  public_ip_address_id      = module.network.public_ip_address_id
+  admin_username            = var.admin_username
+  admin_password            = var.admin_password
+  computer_name             = var.computer_name
+  network_security_group_id = module.network.network_security_group_id
+}
---
+++ variables.tf
@@ -0,0 +1,98 @@
+variable "location" {
+  description = "The Azure region where resources will be deployed"
+  default     = "uksouth"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the Azure resource group"
+  default     = "mate-azure-task-12"
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+  default     = "vnet"
+  type        = string
+}
+
+variable "vnet_address_prefix" {
+  description = "The address space for the virtual network in CIDR notation"
+  default     = "10.0.0.0/16"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet within the virtual network"
+  default     = "default"
+  type        = string
+}
+
+variable "subnet_address_prefix" {
+  description = "The address space for the subnet in CIDR notation"
+  default     = "10.0.0.0/24"
+  type        = string
+}
+
+variable "network_security_group_name" {
+  description = "The name of the network security group"
+  default     = "defaultnsg"
+  type        = string
+}
+
+variable "public_ip_address_name" {
+  description = "The name of the public IP address resource"
+  default     = "linuxboxpip"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  default     = "matebox"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size/SKU of the virtual machine"
+  default     = "Standard_B1s"
+  type        = string
+}
+
+variable "ssh_key_public" {
+  description = "The public SSH key for VM authentication"
+  type        = string
+}
+
+variable "dns_label" {
+  description = "The DNS label prefix for the public IP address"
+  default     = "matetask13579"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the Azure storage account"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "The name of the storage container within the storage account"
+  default     = "task-artifacts"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The administrator username for the virtual machine"
+  default     = "testadmin"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The administrator password for the virtual machine"
+  type        = string
+}
+
+variable "computer_name" {
+  description = "The computer name/hostname for the virtual machine"
+  default     = "hostname"
+  type        = string
+}
