57f6156c4b8e4fd61310852609091474468950f9
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 15:34:30 +0300

    changed HTTP security rule in network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -30,13 +30,13 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "ToDoApp"
+    name                       = "HTTP"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "8080"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
