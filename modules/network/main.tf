3bb48c257f218aa2ecebc4eea77990e185be047e
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 16:08:03 +0300

    formated main.tf in network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_address_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  allocation_method = var.public_ip_allocation_method
+  allocation_method   = var.public_ip_allocation_method
   domain_name_label   = var.dns_label
   sku                 = var.public_ip_sku
 }
