931aa215171c8b82c65b44014779fcf3f6466c7b
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 15:16:14 +0300

    make start script executable in install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -2,10 +2,10 @@
 
 # Script to silently install and start the todo web app on the virtual machine. 
 # Note that all commands bellow are without sudo - that's because extention mechanism 
-# runs scripts under root user. 
+# runs scripts under root user.
 
 # install system updates and isntall python3-pip package using apt. '-yq' flags are 
-# used to suppress any interactive prompts - we won't be able to confirm operation 
+# used to suppress any interactive prompts - we won't be able to confirm operation
 # when running the script as VM extention.  
 apt-get update -yq
 apt-get install python3-pip -yq
@@ -16,6 +16,7 @@ git clone https://github.com/zave52/devops_todolist_terraform_task.git
 cp -r devops_todolist_terraform_task/app/* /app
 
 # create a service for the app via systemctl and start the app
+chmod +x /app/start.sh
 mv /app/todoapp.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start todoapp
